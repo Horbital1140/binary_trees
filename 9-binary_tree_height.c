39569815c369f9726fe000a224123e9b42ab291f
TeeMeeLehin <samemerald8@gmail.com>
2023-08-29 21:00:26 +0100

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
 
 
 size_t binary_tree_height(const binary_tree_t *tree)
-{
+/*{
 size_t flag = 0;
 const binary_tree_t *temp = malloc(sizeof(binary_tree_t));
 if (tree == NULL)
@@ -21,4 +21,20 @@ flag++;
 temp = temp->left ? temp->left : temp->right;
 }
 return (flag);
+}*/
+{
+if (tree == NULL)
+{
+return (0);
+}
+size_t left_node = binary_tree_height(tree->left);
+size_t right_node = binary_tree_height(tree->right);
+if (left_node > right_node)
+{
+return (left_node + 1);
+}
+else
+{
+return (right_node + 1);
+}
 }
